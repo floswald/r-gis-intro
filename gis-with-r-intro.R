786e95a7bc76ad444242229b562e1114aca4a074
florian oswald <florian.oswald@gmail.com>
2019-02-26 08:32:54 +0100

    added stuff

--- gis-with-r-intro.R
+++ gis-with-r-intro.R
@@ -167,6 +167,7 @@ points_sf <- st_as_sf(geo_data, coords = c("lon", "lat"), crs = 4326)
 # Get coastal and country world maps as sf objects
 coast_sf <- ne_coastline(scale = "medium", returnclass = "sf")
 countries_sf <- ne_countries(scale = "medium", returnclass = "sf")
+france_sf <- ne_countries(geounit = "France",type="map_units", returnclass = "sf")
 
 # Subset of locations with "n" greater than 10
 filter(points_sf, n > 10)
@@ -189,9 +190,31 @@ countries_sf %>%
   st_transform(crs = "+proj=moll +datum=WGS84") %>% 
   plot(key.pos = NULL, graticule = TRUE, main = "South America")
 
+# Map of France
+france_sf %>% 
+  select(name) %>% 
+  plot(key.pos = NULL, graticule = TRUE, main = "France")
+
+
 #################################
 ## Mapping with sf and ggplot2 ##
 #################################
+
+f_data <- data.frame(
+  city = c("Paris","Lyon","Marseille","Bordeaux"),
+  lon = c(2.346941, 4.754196,5.380583	,-0.586191),
+  lat = c(48.858884,	45.697190,43.280427,	44.863506	)
+)
+
+f_points_sf <- st_as_sf(f_data, coords = c("lon","lat"), crs=4326)
+# distance to paris
+f_points_sf$dist = st_distance(f_points_sf, subset(f_points_sf,city=="Paris"))
+f_points_sf$dist = units::set_units(f_points_sf$dist,km)
+
+
+ggplot() + 
+  geom_sf(data = france_sf) + 
+  geom_sf(data = f_points_sf, aes(color = as.factor(round(dist,2)))) + geom_sf_label(data = f_points_sf, aes(label = city),nudge_x = 0.5,nudge_y = 0.5)
 
 ### Basic ggplot2 plot with geom_sf ###
 ggplot() + 
@@ -199,8 +222,133 @@ ggplot() +
   geom_sf(data = points_sf,
           aes(color = type, size = n),
           alpha = 0.7,
-          show.legend = "point") +
-  coord_sf(xlim = c(-1, 14), ylim = c(44, 55))
+          show.legend = "point") + coord_sf(xlim = c(-1, 14), ylim = c(44, 55))
+
+# load a shapefile and plot
+sh = st_read("~/Downloads/CONTOURS-IRIS_2-1__SHP__FRA_2018-06-08/CONTOURS-IRIS/1_DONNEES_LIVRAISON_2018-06-00105/CONTOURS-IRIS_2-1_SHP_LAMB93_FXX-2017/CONTOURS-IRIS.shp",stringsAsFactors=FALSE)
+sh_lalo = st_transform(sh,crs = 4326)
+# paris
+p = sh_lalo %>%
+  mutate(INSEE_N = as.integer(INSEE_COM)-75100) %>%
+  filter((INSEE_N < 21) & (INSEE_N > 0))
+
+# using base plot
+#####
+
+# plot paris IRIS inside communes
+p %>%
+  select("INSEE_COM") %>%
+  plot(key.width=lcm(3.5),axes=TRUE)
+
+# aggregating geometry by a feature
+# plot number of iris within a commune
+p %>%
+  group_by(INSEE_COM) %>%
+  summarise(n = n()) %>%
+  select(n) %>%
+  plot(main="Number of Iris within Commune")
+
+# with ggplot and labelling areas
+# strangely, this casts automatically to lat/lon CRS
+p_arr = p %>%
+  group_by(INSEE_N) %>%
+  summarise(n = n(),id=INSEE_N[1])
+p_arr = p_arr %>%
+  mutate(num_IRIS = cut(n,5))
+
+ggplot(p_arr) +
+  geom_sf(aes(fill=num_IRIS)) + 
+  geom_sf_label(aes(label=id)) + 
+  ggtitle("Paris arrondissements with num of IRIS") 
+
+# same and plot centroid
+# does not work 
+# https://stackoverflow.com/questions/54723899/st-centroid-does-not-lie-within-polygon-on-lambert-93-shapefile
+p_arr = p %>%
+  group_by(INSEE_COM) %>%
+  summarise(n = n()) %>%
+  select(n)
+
+# works with pure geometry
+plot(st_geometry(p_arr))
+plot(st_geometry(st_centroid(p_arr)), pch = 3, col = 'red', add = TRUE)
+
+
+# works with ggplot?
+ggplot(p_arr) + geom_sf(aes(fill=NULL)) + geom_sf(data=st_geometry(st_centroid(p_arr)))
+
+
+
+
+
+# generate a fake population weights dataset
+p_arr$weights = sample(1:20,size=20,replace=FALSE)
+
+
+weights$jobs = sample(100:2000, size=20,replace=FALSE)
+centroids = st_centroid(st_geometry(weights))
+sp = as(centroids,'Spatial')
+sdf = SpatialPointsDataFrame(sp,data.frame(weights=weights$jobs))
+library(spatialEco)
+wtd.center = wt.centroid(sdf, "weights")
+wtd.center_sf = st_as_sf(wtd.center)
+
+plot(st_geometry(p_arr))
+plot(st_geometry(wtd.center_sf),pch=3,col="red",add=TRUE)
+plot(st_centroid(st_geometry(p_arr)),pch=3,col="blue",add=TRUE)
+
+wtc <- function(g,w){
+  if (!(is(g,"sf")) | !(w %in% colnames(g))){
+    stop(paste("requires an sf object with at a column",w))
+  }
+  centers = st_coordinates(st_centroid(st_geometry(g)))
+  # crsx = st_crs(g)
+  out = st_point(c(weighted.mean(centers[,"X"],g[[w]]), weighted.mean(centers[,"Y"],g[[w]])))
+  return(out)
+}
+
+plot(st_geometry(p_arr))
+plot(wtc(p_arr,"weights"),add=TRUE,col="red")
+
+
+# works with ggplot?
+center = wtc(p_arr,"weights")
+
+center_sf = st_as_sf(data.frame(lon=center[1],lat=center[2],data = 1),coords = c("lon","lat"), crs=st_crs(p_arr))
+
+ggplot() + geom_sf(data=p_arr) + geom_sf(data=center_sf)
+ggplot() + geom_sf(data=st_geometry(st_union(p_arr))) + geom_sf(data=center_sf)
+
+
+
+nc = st_read(system.file("shape/nc.shp", package="sf"))
+nc$weights = c(rep(1,5),rep(0,95))
+plot(st_geometry(nc))
+plot(st_centroid(st_union(nc)),col="red",pch=3,add=TRUE)
+plot(wtc(nc,"weights"),col="blue",pch=3,add=TRUE)
+
+p_wgs = st_transform(p,4326)
+p_arr_wgs = p_wgs %>%
+  group_by(INSEE_COM) %>%
+  summarise(n = n()) %>%
+  select(n)
+
+plot(p_arr_wgs,axes=TRUE)
+plot(st_centroid(p_arr_wgs), pch = 3, col = 'red', add = TRUE)
+
+
+# SO solution
+p_arr <- sh %>%
+  mutate(INSEE_N = as.integer(INSEE_COM)-75100) %>%
+  filter((INSEE_N < 21) & (INSEE_N > 0)) %>%
+  group_by(INSEE_COM) %>%
+  summarise()
+plot(p_arr)
+plot(st_geometry(st_centroid(p_arr)),add=TRUE)
+
+library(mapview)
+mapview( list( p_arr, st_centroid( p_arr ) ) )
+
 
 ### Plot map with coastlines (lines) data ###
 # Load ggrepel package
